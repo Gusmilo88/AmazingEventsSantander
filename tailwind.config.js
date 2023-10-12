8fd4a6904a20bc64d31d1c75157b0a06048f2915
Gus <gus_eze1988@hotmail.com>
2023-10-11 23:31:50 -0300

    agrega footer y carrusel home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "azulLogo": "#01acfc",
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 }
+
 
